828c2659ec4de5ca5a8612e7262ab60635ef107a
JBC <email@example.com>
2020-10-21 21:34:45 -0400

    fix import

--- src/app/services/rendering-subjects.service.ts
+++ src/app/services/rendering-subjects.service.ts
@@ -1,11 +1,46 @@
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
 import { RenderEvent } from '../model/events/render-event';
+import * as PIXI from 'pixi.js';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RenderingSubjectsService {
   public sceneSubject: Subject<RenderEvent> = new Subject();
-  constructor() { }
+  public app = new PIXI.Application();
+  currentRotation: number = 0.01;
+
+
+  constructor() {
+  }
+
+  public startRendering() {
+    this.app.loader.add('bunny', 'assets/bunny.png').load((loader, resources) => {
+      // This creates a texture from a 'bunny.png' image
+      const bunny = new PIXI.Sprite(resources.bunny.texture);
+
+      // Setup the position of the bunny
+      bunny.x = this.app.renderer.width / 2;
+      bunny.y = this.app.renderer.height / 2;
+
+      // Rotate around the center
+      bunny.anchor.x = 0.5;
+      bunny.anchor.y = 0.5;
+
+      // Add the bunny to the scene we are building
+      this.app.stage.addChild(bunny);
+
+      // Listen for frame updates
+      this.app.ticker.add(() => {
+        // each frame we spin the bunny around a bit
+        bunny.rotation += this.currentRotation;
+      });
+    });
+
+
+  }
+
+
+
 }
--- src/app/services/sprite-repository.service.ts
+++ src/app/services/sprite-repository.service.ts
@@ -6,4 +6,9 @@ import { Injectable } from '@angular/core';
 export class SpriteRepositoryService {
   spriteArray: Array<PIXI.Sprite>;
   constructor() { }
+
+  public addSprite(name: string, path: string) {
+    this.spriteArray.push();
+
+  }
 }
